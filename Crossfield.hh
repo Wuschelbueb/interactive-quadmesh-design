11428c2d25d2b67af143653ecad44af016758b82
wuschelbueb <Wuschelbueb@users.noreply.github.com>
2022-07-05 16:39:34 +0200

    started to document function and classes

--- Crossfield.hh
+++ Crossfield.hh
@@ -21,6 +21,9 @@
 #include <vector>
 #include <float.h>
 
+/**
+ * class is used to create crossfield of each triangle included in selection
+ */
 class Crossfield {
 public:
     using Point = OpenMesh::Vec3d;
@@ -44,61 +47,212 @@ public:
     ~Crossfield() {
     }
 
+    /**
+     * get the crossfield
+     */
     void getCrossfield();
 
 private:
 
+    /**
+     * create crossfield with solution from CoMiSo solver.\n
+     * for each face which is in selection there is a crossfield.\n
+     * @param faces ector of faces of selection
+     */
     void createCrossfields(const std::vector<int> &faces);
 
+    /**
+     * calculate energy.\n
+     * is used check if small examples work properly.\n
+     * @param _A row matrix
+     * @param _x vector with solution
+     * @param _b vector with kappas
+     * @return
+     */
     double
     getEnergy(const RMatrixType &_A, const std::vector<double> &_x, const std::vector<double> &_b);
 
+    /**
+     * gets and creates constraint matrix
+     * @param heKappa map with halfedges and their assigned kappa
+     * @param faces vector of faces of selection
+     * @return constraint matrix
+     */
     gmm::row_matrix<gmm::wsvector<double>>
     getConstraintMatrix(const std::map<int, double> &heKappa, const std::vector<int> &faces);
 
+    /**
+     * create the theta constraints.\n
+     * assign in n_col+1 the theta angle and at the position of the according face a one.\n
+     * increase counter for next iteration
+     * @param n_col size of columns
+     * @param counter decides which row is worked on
+     * @param faceConstraints vector of face constraints
+     * @param _constraints row matrix
+     */
     void getThetaConstraints(const int n_col, int &counter, std::vector<int> &faceConstraints,
                              gmm::row_matrix<gmm::wsvector<double>> &_constraints);
 
+    /**
+     * create periodjump constraints.\n
+     * if two faces have the same origin and one is the predecessor of the other add an entry in the constraint matrix.\n
+     * @param heKappa map with halfedges and their assigned kappa
+     * @param counter decides which row is worked on
+     * @param pj_start at which position (column) periodjump start
+     * @param _constraints row matrix
+     */
     void getPJConstraints(const std::map<int, double> &heKappa, int &counter, const int pj_start,
-                          const int &noOriginConst,
                           gmm::row_matrix<gmm::wsvector<double>> &_constraints);
 
+    /**
+     * gets the number of periodjump constraints a mesh has.\n
+     * i.e. the number of faces which aren't their own origin.\n
+     * @param faces vector of faces of selection
+     * @return the number of periodjump constraints
+     */
     int getAmountPJConstraints(const std::vector<int> &faces);
 
-    std::vector<int> getIdxToRound(const std::map<int, double> &heKappa, const std::vector<int> &faces);
+    /**
+     * gets the indices which need to be rounded which are all periodjump entries.\n     *
+     * @param heKappa map of halfedges with their assigned kappa
+     * @param faces vector of faces of selection
+     * @return amount of indicies to be rounded
+     */
+    std::vector<int> getIdxToRound(const std::map<int, double> &heKappa, int pj_start);
 
+    /**
+     * creates righthandside (b). Ax = b.\n
+     * needed for the constrained solver. is split in two parts.\n
+     * @param heKappa map of halfedges with their assigned kappa
+     * @param faces vector of faces of selection
+     * @return vector of righthandside
+     */
     std::vector<double> getRHS(const std::map<int, double> &heKappa, const std::vector<int> &faces);
 
+    /**
+     * first part of creating rhs vector.\n
+     * handles all the angles (thetas) for the righthandside.\n
+     * each face has a certain amount of kappas attached to it. add them up.\n
+     * @param faces vector of faces of selection
+     * @param _rhs vector of righthandside
+     * @param heKappa map of halfedges with their assigned kappa
+     */
     void getRhsFirstHalf(const std::vector<int> &faces, std::vector<double> &_rhs,
                          const std::map<int, double> &heKappa);
 
+    /**
+     * helper function of the getRhsFirstHalf.\n
+     * iterates through each halfedge of triangle. and adds up the attached kappas\n
+     * @param i index of a face
+     * @param _rhs vector of righthandside
+     * @param heKappa map of halfedges with their assigned kappa
+     */
     void getSum(const int i, std::vector<double> &_rhs, const std::map<int, double> &heKappa);
 
-    double setSum(OpenMesh::FaceHandle fh, OpenMesh::HalfedgeHandle fh_it, const std::map<int, double> &heKappa);
+    /**
+     * extracts the kappa of a halfedge
+     * @param fh_it halfedge
+     * @param heKappa map of halfedges with their assigned kappa
+     * @return kappa
+     */
+    double extractKappa( OpenMesh::HalfedgeHandle fh_it, const std::map<int, double> &heKappa);
 
+    /**
+     * adds kappa of halfedge to the according position of rhs.\n
+     * @param _rhs vector of righthandside
+     * @param heKappa map of halfedges with their assigned kappa
+     * @param facesPlusOne position where on rhs to start
+     */
     void getRhsSecondHalf(std::vector<double> &_rhs,
                           const std::map<int, double> &heKappa, const int facesPlusOne);
 
+    /**
+     * gets matrix A to calculate energy. Ax+b = 0.\n
+     *
+     * @param faces vector of faces of selection
+     * @param heKappa map of halfedges with their assigned kappa
+     * @return row matrix
+     */
     RMatrixType getMatrixA(const std::vector<int> &faces, const std::map<int, double> &heKappa);
 
+    /**
+     * gets vector b to calculate energy. Ax+b = 0.\n
+     * @param heKappa map of halfedges with their assigned kappa
+     * @return
+     */
     std::vector<double> getVectorb(const std::map<int, double> &heKappa);
 
+    /**
+     * get hessian matrix for the CoMiSo solver.\n
+     * there are multiple helper function to create the hessian matrix.\n
+     * @param faces vector of faces of selection
+     * @param heKappa map of halfedges with their assigned kappa
+     * @return column matrix
+     */
     CMatrixType getHessianMatrix(const std::vector<int> &faces, const std::map<int, double> &heKappa);
 
+    /**
+     * multiplies entry of face fh in Hessian matrix by the amount of adjacent faces in the faces vector.\n
+     * this is only relevant for the diagonal of the hessian matrix.\n
+     * helper function of hessian matrix.\n
+     * @param fh face handle
+     * @param faces vector of faces of selection
+     * @return factor which gets multiplied
+     */
     int getFactor(const OpenMesh::FaceHandle fh, const std::vector<int> &faces);
 
+    /**
+     * each face gets a unique number (property) to make sure that the position in the hessian is unique aswell.\n
+     * sometimes not all faces are in selection, that is why we need a numbering.\n
+     * helper function of hessian matrix.\n
+     * @param heKappa map of halfedges with their assigned kappa
+     */
     void setPositionInHessianForFaces(const std::map<int, double> &heKappa);
 
+    /**
+     * creates a map of halfedges and their assigned kappas.\n
+     * @param faces vector containing faces of selection
+     * @return map which contains halfedges and their assigned kappa
+     */
     std::map<int, double> getMapHeKappa(const std::vector<int> &faces);
 
+    /**
+     * checks if the neighbouring face is aswell part of the face selection.\n
+     * helper function of getMapHeKappa.\n
+     * @param fh facehandle
+     * @param fh_neigh neighbouring face handle
+     * @param heKappa map of halfedges with their assigned kappa
+     */
     void getStatusNeigh(const OpenMesh::FaceHandle fh, const OpenMesh::FaceHandle fh_neigh,
                         std::map<int, double> &heKappa);
 
+    /**
+     * adds kappa from getKappa function to the heKappa map if the opposing halfedge wasn't already added.\n
+     * helper function of getMapHeKappa.\n
+     * @param commonEdge pair of two opposing halfedges
+     * @param kappa angle kappa
+     * @param heKappa map of halfedges with the assigned kappa
+     */
     void addKappaHeToMap(const std::pair<int, int> commonEdge, const double kappa, std::map<int, double> &heKappa);
 
+    /**
+     * returns common edge, i.e. the two opposing halfedges between two neighbouring triangles.\n
+     * helper function of getStatusNeigh.\n
+     * @param fh face handle
+     * @param fh_neigh face handle neighbour
+     * @return pair of two opposing halfedges
+     */
     std::pair<int, int>
     getCommonEdgeBetweenTriangles(const OpenMesh::FaceHandle fh, const OpenMesh::FaceHandle fh_neigh);
 
+    /**
+     * calculates the kappa between two reference edges, i.e. two neighbouring faces.\n
+     * helper function of getMapHeKappa.\n
+     * @param refEdgeMain reference Edge of face 1
+     * @param refEdgeNeigh reference Edge of face 2
+     * @param commonEdge opposing halfedges which connect those two faces
+     * @return angle kappa
+     */
     double getKappa(const int refEdgeMain, const int refEdgeNeigh, const std::pair<int, int> commonEdge);
 
     void setVecFieldProp();
@@ -115,6 +269,12 @@ private:
                      const int faceSize, const std::map<int, double> &heKappa,
                      const std::vector<double> &_x);
 
+    /**
+     * rotate crossfield of each triangle with theta from solution vector _x\n
+     * save as rotated as crossfield property
+     * @param faces included in selection
+     * @param _x solution vector from CoMiSo constrained solver
+     */
     void setRotThetaOfVectorField(const std::vector<int> &faces, const std::vector<double> _x);
 
     void colorFaces(const std::vector<int> &faces);
@@ -131,11 +291,13 @@ private:
 
     std::vector<int> getFaceConstraints();
 
+    void setPJProp(std::map<int, double> &heKappa, std::vector<double> &_x, const int faceSize);
+
     TriMesh &trimesh_;
     std::vector<int> &heInRange_;
     std::vector<int> &heConstraints_;
 
-    void setPJProp(std::map<int, double> &heKappa, std::vector<double> &_x, const int faceSize);
+
 };
 
 
--- DijkstraDistance.hh
+++ DijkstraDistance.hh
@@ -13,12 +13,12 @@
 #include <float.h>
 #include <algorithm>
 
-
+/**
+ * class which uses the dijkstra algorithm on the mesh with various different conditions.
+ */
 class DijkstraDistance {
 public:
     using Point = ACG::Vec3d;
-
-    void cleanMeshOfProps();
 
 public:
     DijkstraDistance(TriMesh &trimesh) : trimesh_{trimesh} {}
@@ -27,57 +27,186 @@ public:
 
 public:
 
+    /**
+     * removes all the custom properties from the mesh. This function only gets used at the start.\n
+     * This makes sure that there are no old values which could interfere with new calculations.
+    */
+    void cleanMeshOfProps();
+
+    /**
+     * creates a dualGraph (dual spanning tree) starting from a random face from faces vector
+     * additionally divides edges in three categories:\n
+     * - outside of selection\n
+     * - inside of selection\n
+     * - on the border\n
+     * @param faces
+     */
     void getDualGraph(const std::vector<int> &faces);
 
-    void completeDijkstraWSingularities(std::vector<int> &complementHEdges,
-                                        std::vector<int> &singularities, std::vector<int> &cutGraphWoBoundary);
+    /**
+     * uses complementHEdges and singularities to create the cutgraph with the help of Dijkstra.\n
+     * adds halfedges to complementHEdges and cutGraphWoBoundary which get used again later.\n
+     * @param complementHEdges edges which are not part of the dual spanning tree which got calculated with the getDualGraph
+     * @param singularities vertices with a crossfield index != 0
+     * @param cutGraphWoBoundary same vector as complementHEdges but without borders/boundaries
+     */
+    void calcDijkstraWSingularities(std::vector<int> &complementHEdges,
+                                    std::vector<int> &singularities, std::vector<int> &cutGraphWoBoundary);
 
+    /**
+     * calculate Dijkstra in order to get selection which gets used for quadrangulation
+     * @param HeConstraints halfedges which are constrained, dijkstra starts from these
+     * @param refDist distance on how far the dijkstra algo needs to spread out
+     * @param includeBoundary flag decides if it should include faces which are on the edge of refDist
+     * @return
+     */
     std::vector<int>
     calculateDijkstra(const std::vector<int> HeConstraints, const double refDist, const bool includeBoundary);
 
-    std::vector<int> getHeVectorOfSelection(const std::vector<int> &includedNodes);
+    /**
+     * returns a vector of halfedges from dijkstra calculation .
+     * @param includedFaces is a vector which contains all the faces from dijkstra calculation
+     * @return a vector of halfedges
+     */
+    std::vector<int> getHeVectorOfSelection(const std::vector<int> &includedFaces);
 
+    /**
+     * colorizes edges which got included in dijkstra calculation
+     * @param includedHEdges vector of Hedges in selection
+     */
     void colorizeEdges(const std::vector<int> &includedHEdges);
 
+    /**
+     * get halfedges which got selected with the selection tool
+     * @return a vector with halfedges
+     */
     std::vector<int> getHeConstraints();
 
 private:
 
+    /**
+     * gets the smallest distance (face property) from the origin.\n
+     * this is how dijkstra propagates
+     * @param faces checks all the faces and their distance
+     * @return returns idx of face with shortest distance
+     */
     int dualGraphGetSmallestDist(const std::vector<int> &faces);
 
-    void getBorder();
+    /**
+     * colors DualGraph. There are 3 colors\n
+     * 1 is for the border\n
+     * 2 is for the selection inside the border
+     * 3 is for the selection outside of the border
+     */
+    void colorDualGraph();
 
+    /**
+     * helper function for colorDualGraph. colorizes common edge between face "fh" and face "fh1"\n
+     * @param fh
+     * @param fh2
+     * @param color based on requirements in colorDualGraph() function
+     */
     void searchComEBetweenF(const OpenMesh::FaceHandle fh, const OpenMesh::SmartFaceHandle fh2, const int color);
 
+    /**
+     * initialize different properties which get used to create the DualGraph
+     * @param faces
+     */
     void initDualGraphProp(const std::vector<int> &faces);
 
+    /**
+     * creates DualGraph with the help of the Dijkstra algorithm
+     * @param faces
+     */
     void calculateDGDijkstra(const std::vector<int> &faces);
 
+    /**
+     * creates vector of vertices. takes complementHEdges and singularities as input.\n
+     * extracts from these two vectors the vertices
+     * @param complementHEdges vector of halfedges
+     * @param singularities vector of vertices
+     * @return vector of vertices
+     */
     std::vector<int> createVerticesVector(std::vector<int> &complementHEdges, std::vector<int> &singularities);
 
+    /**
+     * initializes vertex properties which then get used to calculate the cutgraph with the help of dijsktra.\n
+     * @param cutGraphVertices vector of vertices
+     * @param first_it this flag is only true for the first use of function
+     */
     void initProperties(std::vector<int> &cutGraphVertices, const bool first_it);
 
+    /**
+     * get smallest distance of vertex selection. is used as a helper function of dijkstra.\n
+     * @return return vertex with smallest distance
+     */
     int vertexGetSmallestDist();
 
+    /**
+     * directed dijkstra calculation with vertices. distance between vertices is always 1.\n
+     * has always the goal of reaching the singularity.\n
+     * @param i is a index of a singularity
+     */
     void calculateVDijkstra(const int i);
 
+    /**
+     * when dijkstra stops path to the singularity gets added to complementHEdges and cutGraphWoboundary.\n
+     * vertex of singularity gets added to cutGraphVertcies for the next dijkstra iteration.
+     * @param cutGraphVertices vector which gets used for dijkstra iteration
+     * @param complementHEdges vector of halfedges
+     * @param i singularity
+     * @param cutGraphWoBoundary vector of halfedges
+     */
     void addPathToCutGraph(std::vector<int> &cutGraphVertices, std::vector<int> &complementHEdges, const int i,
                            std::vector<int> &cutGraphWoBoundary);
 
+    /**
+     * includes faces which got cut by refDist but not included.\n
+     * adds the "border" faces to includedFaces.
+     * @param includedFaces vector of faces
+     * @param refDist reference Distance
+     */
     void includeBoundaryFaces(std::vector<int> &includedFaces, const double refDist);
 
+    /**
+     * transforms constraintHeh vector to a face vector. faces which are assigned to halfedges\n
+     * initializes a lot of properties.\n
+     * @param constraintHeh vector of halfedges
+     * @return vector of faces
+     */
     std::vector<int> transformHehToFaces(const std::vector<int> &constraintHeh);
 
-    void dijkstraDistBaryCenter(std::vector<int> &includedNodes, const double refDist);
+    /**
+     * calculates Dijkstra. uses distance from one face (barycenter) to another.\n
+     * @param includedFaces vector which contains faces which get added by dijkstra
+     * @param refDist reference distance
+     */
+    void dijkstraDistBaryCenter(std::vector<int> &includedFaces, const double refDist);
 
+    /**
+     * is a helper function of the Dijkstra calculation. gets the face with the smallest distance.\n
+     * the distance can't be bigger than the reference distance.
+     * @param refDist reference distance
+     * @return index of face
+     */
     int getSmallestDistProp(const double refDist);
 
-    void getSelectedVertices(std::vector<int> &constraints);
-
+    /**
+     * gets selection of edges as halfedges.\n
+     * @param constraints vector which contains the selection as halfedges
+     */
     void getSelectedEdges(std::vector<int> &constraints);
 
+    /**
+     * gets selection of halfedges.
+     * @param constraints vector which contains the selection as halfedges
+     */
     void getSelectedHEdges(std::vector<int> &constraints);
 
+    /**
+     * gets selection of faces and saves the halfedges of face.\n
+     * @param constraints vector which contains the selection as halfedges
+     */
     void getSelectedFaces(std::vector<int> &constraints);
 
     TriMesh &trimesh_;
